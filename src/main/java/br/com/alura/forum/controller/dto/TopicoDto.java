7547276c6ccced82219dfddc87cf27b4f26a343e
flavio <flaviocr2012@gmail.com>
2021-07-27 10:10:19 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,46 @@
+package br.com.alura.forum.controller.dto;
+
+import java.awt.font.ShapeGraphicAttribute;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+
+public class TopicoDto {
+
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+	
+	public TopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+		// Pega cada registro em tópico e transforma em tópicoDTO
+		return topicos.map(TopicoDto::new);
+	}
+
+}

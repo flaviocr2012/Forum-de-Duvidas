9e87285b8032687b7d1abe08fc20ecd990b5addc
flavio <flaviocr2012@gmail.com>
2021-07-29 12:11:47 -0300

    Documentação de API Swagger

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport // O Spring pega os parâmetros de paginação e ordenação da WEB e repassa pro SpringxData
 @EnableCaching // Habilita o uso de cache na aplicação.
+@EnableSwagger2
 public class ForumApplication {
 
 	public static void main(String[] args) {
